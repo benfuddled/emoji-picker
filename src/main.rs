e314f7bdaf57aed035e1e5b8b061da9262cd9483
Benjamin Richardson <21092217+benfuddled@users.noreply.github.com>
2024-03-02 23:41:32 -0500

    reactivity!!!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use iced::executor;
-use iced::widget::{column, container, slider, text, vertical_space};
-use iced::{Application, Command, Element, Settings, Theme };
+use iced::widget::{column, container, slider, text, vertical_space, Column, checkbox};
+use iced::{Application, Command, Element, Settings, Theme};
 use emojis;
 use iced::widget::Slider;
 
@@ -10,11 +10,13 @@ pub fn main() -> iced::Result {
 
 struct Picker {
     happiness_level: f32,
+    show_it: bool,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    HappinessLevelChanged(u8)
+    HappinessLevelChanged(u8),
+    ToggleTwo(bool)
 }
 
 impl Application for Picker {
@@ -25,7 +27,8 @@ impl Application for Picker {
 
     fn new(_flags: ()) -> (Picker, Command<Self::Message>) {
         (Picker {
-            happiness_level: 10.0
+            happiness_level: 10.0,
+            show_it: false
         }, Command::none())
     }
 
@@ -40,6 +43,11 @@ impl Application for Picker {
 
                 Command::none()
             }
+            Message::ToggleTwo(show_it) => {
+                self.show_it = show_it;
+
+                Command::none()
+            }
         }
     }
 
@@ -50,7 +58,20 @@ impl Application for Picker {
             self.happiness_level as u8,
             Message::HappinessLevelChanged,
         );
-        let column = column![text(rocket).size(self.happiness_level).shaping(text::Shaping::Advanced), text(self.happiness_level), happiness_slider];
-        container(column).padding(20).center_x().center_y().into()
+        let show_two = checkbox("show two", self.show_it).on_toggle(Message::ToggleTwo);
+        let column = column![show_content(self.happiness_level, self.show_it), show_two, text(self.happiness_level), happiness_slider];
+        // container(column).padding(20).center_x().center_y().into()
+        //let column = column![show_content(self.happiness_level)];
+        container(column).into()
     }
+}
+
+fn show_content<'a>(happiness_level: f32, show_it: bool) -> Element<'a, Message> {
+    let rocket = emojis::get("🚀").unwrap();
+    let text_thing = text(rocket).size(happiness_level).shaping(text::Shaping::Advanced);
+    let mut col = Column::new().push(text_thing);
+    if show_it == true {
+        col = col.push(text(rocket).size(happiness_level).shaping(text::Shaping::Advanced));
+    }
+    col.into()
 }
