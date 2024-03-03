7cbaaf11970aba0777cb8dfb34a246f04f611277
Benjamin Richardson <21092217+benfuddled@users.noreply.github.com>
2024-03-02 21:41:44 -0500

    Add iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::executor;
+use iced::{Application, Command, Element, Settings, Theme};
+
+pub fn main() -> iced::Result {
+    Hello::run(Settings::default())
+}
+
+struct Hello;
+
+impl Application for Hello {
+    type Executor = executor::Default;
+    type Message = ();
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
+        (Hello, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool application")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        "Hello, world!".into()
+    }
 }
