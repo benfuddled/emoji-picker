d78381b754663934bf1871472dbec0708036da43
Benjamin Richardson <21092217+benfuddled@users.noreply.github.com>
2024-03-02 22:49:18 -0500

    rocket resized

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,56 @@
 use iced::executor;
-use iced::{Application, Command, Element, Settings, Theme};
+use iced::widget::{column, container, slider, text, vertical_space};
+use iced::{Application, Command, Element, Settings, Theme };
+use emojis;
+use iced::widget::Slider;
 
 pub fn main() -> iced::Result {
-    Hello::run(Settings::default())
+    Picker::run(Settings::default())
 }
 
-struct Hello;
+struct Picker {
+    happiness_level: f32,
+}
 
-impl Application for Hello {
+#[derive(Debug, Clone)]
+enum Message {
+    HappinessLevelChanged(u8)
+}
+
+impl Application for Picker {
     type Executor = executor::Default;
-    type Message = ();
+    type Message = Message;
     type Theme = Theme;
     type Flags = ();
 
-    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
-        (Hello, Command::none())
+    fn new(_flags: ()) -> (Picker, Command<Self::Message>) {
+        (Picker {
+            happiness_level: 10.0
+        }, Command::none())
     }
 
     fn title(&self) -> String {
-        String::from("A cool application")
+        String::from("Picker")
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::HappinessLevelChanged(level) => {
+                self.happiness_level = level as f32;
+
+                Command::none()
+            }
+        }
     }
 
     fn view(&self) -> Element<Self::Message> {
-        "Hello, world!".into()
+        let rocket = emojis::get("🚀").unwrap();
+        let happiness_slider = slider(
+            0..=100,
+            self.happiness_level as u8,
+            Message::HappinessLevelChanged,
+        );
+        let column = column![text(rocket).size(self.happiness_level).shaping(text::Shaping::Advanced), text(self.happiness_level), happiness_slider];
+        container(column).padding(20).center_x().center_y().into()
     }
 }
