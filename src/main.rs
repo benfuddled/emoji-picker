eae14e43b892677c19a7160a4c506c222c780261
Benjamin Richardson <21092217+benfuddled@users.noreply.github.com>
2024-03-24 15:31:19 -0400

    DELETE UNUSED CODE and adjust search box padding

--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,11 @@ pub fn main() -> iced::Result {
 }
 
 struct Picker {
-    happiness_level: f32,
-    show_it: bool,
     search_val: String
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    HappinessLevelChanged(u8),
-    ToggleTwo(bool),
     SearchInput(String),
     EmojiPressed(String)
 }
@@ -46,8 +42,6 @@ impl Application for Picker {
 
     fn new(_flags: ()) -> (Picker, Command<Self::Message>) {
         (Picker {
-            happiness_level: 24.0,
-            show_it: false,
             search_val: String::from("")
         }, Command::none())
     }
@@ -58,16 +52,6 @@ impl Application for Picker {
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            Message::HappinessLevelChanged(level) => {
-                self.happiness_level = level as f32;
-
-                Command::none()
-            }
-            Message::ToggleTwo(show_it) => {
-                self.show_it = show_it;
-
-                Command::none()
-            }
             Message::SearchInput(input) => {
                 self.search_val = input;
                 println!("{}", self.search_val);
@@ -81,63 +65,21 @@ impl Application for Picker {
     }
 
     fn view(&self) -> Element<Self::Message> {
-        let happiness_slider = slider(
-            0..=100,
-            self.happiness_level as u8,
-            Message::HappinessLevelChanged,
-        );
-        let text_input = text_input("search!!", &*self.search_val).on_input(Message::SearchInput);
-        let show_two = checkbox("show two", self.show_it).on_toggle(Message::ToggleTwo);
-        let column = column![show_content_grid(self.happiness_level, self.show_it, &self.search_val), show_two, text(self.happiness_level), happiness_slider].width(Length::Fill);
-        let scroll_me = Scrollable::new(column).height(Length::Fill);
+        let text_input = text_input("Search", &*self.search_val).on_input(Message::SearchInput);
+        let column = column![show_content_grid(&self.search_val)].width(Length::Fill);
+        let scroll_grid = Scrollable::new(column).height(Length::Fill);
 
-        let scroll_container = Container::new(scroll_me).height(Length::Fill);
-        let text_container = Container::new(text_input);
+        let scroll_container = Container::new(scroll_grid).height(Length::Fill);
+        let text_container = Container::new(text_input).padding(Padding::from([12, 25, 8, 14]));
 
         let window_column = column![text_container, scroll_container];
-        // container(column).padding(20).center_x().center_y().into()
-        //let column = column![show_content(self.happiness_level)];
         container(window_column).into()
     }
 }
 
-fn show_content<'a>(happiness_level: f32, show_it: bool, search_val: &str) -> Element<'a, Message> {
-    let rocket = emojis::get("🚀").unwrap();
-    let stars = emojis::get("🤩").unwrap();
-    let grape = emojis::get("🍇").unwrap();
-    let text_thing = text(rocket).size(happiness_level).shaping(text::Shaping::Advanced);
-    let mut col = Column::new().push(text_thing);
-    if show_it == true {
-        col = col.push(text(stars).size(happiness_level).shaping(text::Shaping::Advanced));
-    }
-    if search_val == "grape" {
-        col = col.push(text(grape).size(happiness_level).shaping(text::Shaping::Advanced));
-    }
-    col.into()
-}
+fn show_content_grid<'a>(search_val: &str) -> Element<'a, Message> {
 
-fn show_content_grid<'a>(happiness_level: f32, show_it: bool, search_val: &str) -> Element<'a, Message> {
-    let rocket = emojis::get("🚀").unwrap();
-    let stars = emojis::get("🤩").unwrap();
-    //let grape = emojis::get("🍇").unwrap();
-    let text_thing = text(rocket).size(happiness_level).shaping(text::Shaping::Advanced);
-    let mut col = Column::new().push(text_thing).width(Length::Fill);
-    // if show_it == true {
-    //     col = col.push(text(stars).size(happiness_level).shaping(text::Shaping::Advanced));
-    // }
-    // if search_val == "grape" {
-    //     col = col.push(text(grape).size(happiness_level).shaping(text::Shaping::Advanced));
-    // }
-
-    // let mojis = emojis::iter().filter(|e| e.unicode_version() < emojis::UnicodeVersion::new(13, 0));
-    //
-    // for e in mojis {
-    //     if search_val.len() <= 0 {
-    //         col = col.push(text(e).size(happiness_level).shaping(text::Shaping::Advanced));
-    //     } else if e.name().contains(search_val) {
-    //         col = col.push(text(e).size(happiness_level).shaping(text::Shaping::Advanced));
-    //     }
-    // }
+    let mut col = Column::new();
 
     // Library recommends to filter the list by the maximum Unicode version that you wish to support.
     let minimum_moji = emojis::iter().filter(|e| e.unicode_version() < emojis::UnicodeVersion::new(13, 0)).collect::<Vec<_>>();
@@ -148,12 +90,10 @@ fn show_content_grid<'a>(happiness_level: f32, show_it: bool, search_val: &str) 
     for moji_row in filtered_moji.chunks(6) {
         let mut row: Row<Message> = Row::new().padding(4).spacing(8);
         for moji in moji_row {
-            let txt = text(moji).size(happiness_level).shaping(text::Shaping::Advanced);
+            let txt = text(moji).size(24.0).shaping(text::Shaping::Advanced);
             let txt_container = Container::new(txt).center_x().width(iced::Length::Fill);
             let btn = Button::new(txt_container).on_press(Message::EmojiPressed(String::from(moji.as_str()))).width(Pixels(75.0));
             let btn_container = Container::new(btn).center_x().width(Pixels(75.0));
-            // row = row.push(button(txt_container)
-            //     .on_press(Message::EmojiPressed(String::from(moji.as_str()))));
 
             row = row.push(btn_container);
         }
